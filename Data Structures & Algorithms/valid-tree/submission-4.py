ffb7f532be899e5e8c0732163426b38191783119
Ridhima Morampudi <99146859+ridhimamorampudi@users.noreply.github.com>
2026-09-06 16:29:05 -0700

    Add: valid-tree - submission-4

---
+++ Data Structures & Algorithms/valid-tree/submission-4.py
@@ -0,0 +1,46 @@
+class UnionFind:
+    def __init__(self,n):
+        self.parent = [i for i in range(n)]
+        self.size = [1]*n
+        self.comps = n
+
+    def find(self,x):
+        if x != self.parent[x]:
+            self.parent[x] = self.find(self.parent[x])
+        return self.parent[x]
+
+    def union(self,x,y):
+        rootX = self.find(x)
+        rootY = self.find(y)
+
+        if rootX == rootY:
+            return False
+        self.comps -= 1
+        if self.size[rootX] < self.size[rootY]:
+            self.parent[rootX] = rootY
+            self.size[rootY] += self.size[rootX]
+        else:
+            self.parent[rootY] = rootX
+            self.size[rootX] += self.size[rootY]
+        return True
+    def components(self):
+        return self.comps
+
+
+class Solution:
+    def validTree(self, n: int, edges: List[List[int]]) -> bool:
+        if len(edges) > n-1:
+            return False
+        
+        uf = UnionFind(n)
+
+        for u,v in edges:
+            if not uf.union(u,v):
+                return False
+            uf.union(u,v)
+        
+
+        return True if uf.components() == 1 else False
+
+        
+
