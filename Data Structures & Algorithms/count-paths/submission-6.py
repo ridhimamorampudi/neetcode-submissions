ca1b601217e125f5de710ef463f6a6557dd5095f
Ridhima Morampudi <99146859+ridhimamorampudi@users.noreply.github.com>
2026-09-07 00:13:42 -0700

    Add: count-paths - submission-6

---
+++ Data Structures & Algorithms/count-paths/submission-6.py
@@ -0,0 +1,23 @@
+class Solution:
+    def uniquePaths(self, m: int, n: int) -> int:
+        grid = [[0]*n for i in range(m)]
+        grid[0] = [1]*n
+        
+
+        for i in range(1,m):
+            for j in range(n):
+                if j == 0:
+                    grid[i][j] = 1
+                    continue
+
+                if 0<=i-1<m:
+                    grid[i][j] += grid[i-1][j]
+                if 0<= j-1<n:
+                    grid[i][j] += grid[i][j-1]
+
+                    
+                
+        return grid[m-1][n-1]
+                 
+
+        
